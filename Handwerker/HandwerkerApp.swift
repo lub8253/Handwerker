6b956dff1c4036fbaeaf2f25e050a99219a32a83
Lukas <lub8253@thi.de>
2025-09-26 17:48:38 +0200

    Initial Commit

---
+++ Handwerker/HandwerkerApp.swift
@@ -0,0 +1,17 @@
+//
+//  HandwerkerApp.swift
+//  Handwerker
+//
+//  Created by Lukas Brand on 26.09.25.
+//
+
+import SwiftUI
+
+@main
+struct HandwerkerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
